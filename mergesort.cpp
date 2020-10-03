7ba84b0c2947bd8e1b9a73f4bb4948f2f81ea1fc
anujkhaire <46042959+anujkhaire@users.noreply.github.com>
2020-10-03 19:53:41 +0530

    added merge function

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,33 +1,71 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int merge(){
+void merge(int h,int mid, int l,int arr[])
+{
+	int s1 = mid - l + 1;
+	int s2 = h - mid;
+	int L[s1],R[s2];
 	
-
-}
-
-void mergesort(int arr[],int low,int high){
 	
-	if(low<high){
+	for(int i=0; i<s1; ++i){
+		L[i]=arr[l+i];	
+	}
+	for(int i=0; i<s2;++i){
+		R[i]=arr[mid+1+i];
+	}
 
-		int mid = low+(high-low) / 2;
+	int i=0,j=0,k=l;
 	
-		mergesort(arr,low,mid);
-		mergesort(arr,mid+1,high);
-		merge(arr,low,high);	
+	while(i<s1 && j<s2){
+		if(L[i]<=R[j]){
+			arr[k]=L[i];
+			++i;
+		}
+		else{
+			arr[k]=R[j];
+			++j;
+		}
+		++k;
+	}
+
+	while(i<s1){
+		arr[k]=L[i];
+		++i,++k;
+	}
+	while(j<s2){
+		arr[k]=R[j];
+		++j,++k;
 	}
 
 }
 
-int main(){
-
-	int n;
-	cin>>n;
-	int arr[n];
-	for(int i=0; i<n;++i){
-		cin>>arr[i];
-	}
+int mergesort(int l,int h,int arr[])
+{
 	
-	mergesort(arr,0,n-1);
+
+	if(l<h){
+		int mid = l + (h - l) / 2;
+		mergesort(l,mid,arr);
+		mergesort(mid+1,h,arr);
+	
+		merge(h,mid,l,arr);
+	}
+
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int arr[5] = {2,1,5,2,0};
+	int arrsize = sizeof(arr)/sizeof(arr[0]);
+
+	mergesort(0,arrsize-1,arr);
+	
+	for(auto x:arr){
+		cout<<x<<"\t";
+	}
 }
