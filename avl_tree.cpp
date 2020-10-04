6cef83ff6ba1317955b8becf6e17241b594f7514
protoc01 <72308217+protoc01@users.noreply.github.com>
2020-10-04 17:36:14 +0530

    Create avl_tree.cpp

---
+++ avl_tree.cpp
@@ -0,0 +1,20 @@
+class avlTree
+{
+    public:
+        int height(avl_node *);
+        int diff(avl_node *);
+        avl_node *rr_rotation(avl_node *);
+        avl_node *ll_rotation(avl_node *);
+        avl_node *lr_rotation(avl_node *);
+        avl_node *rl_rotation(avl_node *);
+        avl_node* balance(avl_node *);
+        avl_node* insert(avl_node *, int );
+        void display(avl_node *, int);
+        void inorder(avl_node *);
+        void preorder(avl_node *);
+        void postorder(avl_node *);
+        avlTree()
+        {
+            root = NULL;
+        }
+};
